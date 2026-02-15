f4191a047b17823f8863893814491e5e164acd74
Devang Singh <devangsinghchauhan933@gmail.com>
2026-02-15 12:59:27 +0530

    update

---
+++ vite-react/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from './chai.jsx'
+
+function App() {
+  const newLine = "Come and take it"
+
+  return (
+    <>
+      <Chai />
+      <h2>{newLine}</h2>
+    </>
+  )
+}
+
+export default App
---
+++ vite-react/src/chai.jsx
@@ -0,0 +1,7 @@
+function Chai() {
+    return (
+        <h1>Chai is ready to be served!</h1>
+    )
+}
+
+export default Chai;
